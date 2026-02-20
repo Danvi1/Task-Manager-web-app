3a034f7f644db9bc93093ba11969ac03d5731306
dhnas <danydhanvith@gmail.com>
2026-02-20 10:53:38 +0530

    using for and if in template

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,21 +2,29 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('task-manager-app');
 
-  users = signal(DUMMY_USERS);
+  users = DUMMY_USERS;
+  selectedUserId?: string;
+
+  get selectedUser(){
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
 
   onSelectUser(id: string){
-    console.log("Selected user with id: ", id)
+    console.log("Selected user with id: ", id);
+    this.selectedUserId = id;
   }
+
 }
