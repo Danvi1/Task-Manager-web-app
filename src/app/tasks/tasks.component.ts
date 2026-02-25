3be961ee6cc858613639fafe34f0bc092e01c813
Dhanvi1 <danydhanvith@gmail.com>
2026-02-25 11:05:17 +0530

    using add new task component in task component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,29 +2,46 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 
 import { DummyTasks } from './dummy-tasks'
+import { NewTaskComponent } from "./new-task/new-task.component";
+import { type NewTaskData } from './task/task.model';
 
 @Component({
   selector: 'app-tasks',
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
     @Input() name?: string;
-    @Input() id?: string;
-    @Output() addNewTask = new EventEmitter<boolean>();
+    @Input() userId!: string;
+    isAddingTask = false;
     
     tasks = DummyTasks;
 
     get selectedUserTasks() {
-        return this.tasks.filter((task) => task.userId === this.id )
+        return this.tasks.filter((task) => task.userId === this.userId )
     }
 
     onCompleteTask(id: string){
         this.tasks = this.tasks.filter((task) => task.id != id);
     }
 
-    addTask() {
-        this.addNewTask.emit(true);
+    onStartAddTask() {
+        this.isAddingTask = true;
+    }
+
+    onCancelAddTask(){
+        this.isAddingTask = false;
+    }
+
+    addNewTask(taskData: NewTaskData){
+        this.tasks.push({
+            id: new Date().getTime().toString(),
+            userId: this.userId?.toString(),
+            title: taskData.title,
+            summary: taskData.summary,
+            dueDate : taskData.date
+        });
+        this.isAddingTask = false;
     }
 }
